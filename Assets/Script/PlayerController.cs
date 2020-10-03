3180d484843c1626ad95744188b813ba8e035b4e
santiagosant <santidevargas@gmail.com>
2020-10-03 14:45:50 -0300

    implementadas animaciones movimiento-ataque-bloquear

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -6,14 +6,18 @@ public class PlayerController : MonoBehaviour
 {
     Animator anim;
     Rigidbody2D rb2d;
-    public float fuerzaSalto;
-    public bool enPiso;
     public Transform refPie;
+    public float fuerzaSalto;
+    public float velX;
+    public bool enPiso;
+    SpriteRenderer sprite;
+    bool ataque;
 
     void Start()
     {
         anim = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
 
     }
 
@@ -25,12 +29,42 @@ public class PlayerController : MonoBehaviour
         //Boolean para animator
         anim.SetBool("enPiso", enPiso);
         // Saltar
-        if (Input.GetKeyDown(KeyCode.J) && enPiso)
+        if (Input.GetKeyDown(KeyCode.W) && enPiso)
         {
+            //Si el KeyDownW es false siempre que se despegue del piso va a hacer al animacion de caida
+            anim.SetBool("KeyDownW", true);
             rb2d.AddForce(new Vector2(0, fuerzaSalto), ForceMode2D.Impulse);
         }
+        //Animacion de ataque
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            anim.SetTrigger("Ataque");
+        }
+        //Animacion de bloqueo
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            anim.SetTrigger("Bloquear");
+        }
 
-        
+    }
+
+    private void FixedUpdate()
+    {
+        //Si esta en el piso KeyDownW es False
+        //si es falso piso  
+        if (!enPiso)
+        {
+            anim.SetBool("KeyDownW", false);
+        }
+
+        //Para movimiento horizontal
+        float movX;
+        movX = Input.GetAxis("Horizontal");
+        anim.SetFloat("AbsMovX",Mathf.Abs(movX));
+        rb2d.velocity = new Vector2(velX * movX, rb2d.velocity.y);
+
+        if (movX < 0) sprite.flipX = true;
+        if (movX > 0) sprite.flipX = false;
 
     }
 }
